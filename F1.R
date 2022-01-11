48d7787b5c0f03ba7a24a38c26da3c5e43cbc393
jsmvk <69394528+jsmvk@users.noreply.github.com>
2022-01-11 13:14:04 +0100

    Delete F1.R

--- F1.R
+++
@@ -1,6 +0,0 @@
-# colnames(pit_strategy) <- c("driverId", "stop", "lap", "duration")
-# 
-# colnames(ham) <- c("driverId", "lap", "position", "time", "name", "year", "surname")
-# 
-# colnames(ver) <- c("driverId", "lap", "position", "time", "name", "year", "surname")
-
